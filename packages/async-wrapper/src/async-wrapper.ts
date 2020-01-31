9a49cce8f41009090a406ee4825b80bedf9ae5bd
Xavier HEN <uminily@gmail.com>
2020-01-31 13:53:03 +0100

    fix(async-wrapper versionning):

--- packages/async-wrapper/src/async-wrapper.ts
+++ packages/async-wrapper/src/async-wrapper.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express'
 
+/**
+ *
+ * @param fn
+ */
 export default function asyncWrapper(fn: RequestHandler) {
   return function(req: Request, res: Response, next: NextFunction) {
     fn(req, res, next).catch(next)
