8978d04ec4207503f1729c891dc9d7f1e5b8044c
Xavier HEN <uminily@gmail.com>
2020-03-09 16:21:12 +0100

    fix(private key secret on firebase tests):

--- packages/uploader/test/mocks/firebase/app.ts
+++ packages/uploader/test/mocks/firebase/app.ts
@@ -11,7 +11,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
   }),
   databaseURL: process.env.FIREBASE_DATABASE_URL
 })
