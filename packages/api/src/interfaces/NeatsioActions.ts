d1f739cb66da12264c190c3eca751c07524b6546
Xavier HEN <uminily@gmail.com>
2020-11-30 15:12:39 +0100

    fix(api): add missing neatsioActions

--- packages/api/src/interfaces/NeatsioActions.ts
+++ packages/api/src/interfaces/NeatsioActions.ts
@@ -1,6 +1,9 @@
 export enum NeatsioActions {
   GET_ONE = 'neatsioGetOne',
   GET_MANY = 'neatsioGetMany',
+  QUERY = 'neatsioQuery',
+  QUERY_COUNT = 'neatsioQueryCount',
+  COUNT = 'neatsioCount',
   CREATE_ONE = 'neatsioCreateOne',
   CREATE_BULK = 'neatsioCreateBulk',
   UPDATE_ONE = 'neatsioUpdateOne',
