43e441de7500039dcd5f164993dce08e970c786b
Xavier HEN <uminily@gmail.com>
2020-12-01 10:49:10 +0100

    style(querier): clean code

--- packages/querier/src/querier.ts
+++ packages/querier/src/querier.ts
@@ -1,22 +1,6 @@
 import * as qs from 'query-string'
 
-const cleanObject = (obj: any) => {
-  Object.keys(obj).forEach(key => {
-    if (obj[key] && typeof obj[key] === 'object') return cleanObject(obj[key])
-
-    if (obj[key] === undefined) {
-      delete obj[key]
-      return
-    }
-
-    if (typeof obj[key] === 'string' && !obj[key].length) {
-      delete obj[key]
-      return
-    }
-  })
-
-  return obj
-}
+import { cleanObject } from './utils'
 
 /**
  *
