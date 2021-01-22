2a2ea86fd774e693bab488c0b006a58b21bd7b1f
Xavier HEN <uminily@gmail.com>
2021-01-22 10:56:27 +0100

    fix(api): add static instance to service decorator

--- packages/api/src/decorators/service.decorator.ts
+++ packages/api/src/decorators/service.decorator.ts
@@ -1,6 +1,13 @@
-export const Service = (): ClassDecorator => {
-  return target => {
+import { Injector } from '../di/Injector'
+
+export const Service = <T extends { new (...args: any[]): any }>() => {
+  return (constructor: T) => {
     // TODO: save into a provider container
-    return target
+
+    const serviceClass: any = class ServiceConstructor extends constructor {
+      public static instance = Injector.resolve<T>(constructor)
+    }
+
+    return serviceClass
   }
 }
