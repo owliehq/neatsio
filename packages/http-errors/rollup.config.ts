62e9bf863f762d66f5bbecafe9411de0f6db5586
Xavier HEN <uminily@gmail.com>
2021-02-01 15:28:49 +0100

    fix(http-errors): rollup config with new files

--- packages/http-errors/rollup.config.ts
+++ packages/http-errors/rollup.config.ts
@@ -4,13 +4,13 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'http-errors'
+const libraryName = 'index'
 
 export default {
   input: `src/${libraryName}.ts`,
   output: [
     //{ file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'cjs', sourcemap: true }
+    { file: 'dist/index.js', format: 'cjs', sourcemap: true }
   ],
   external: ['mongoose', 'express', 'pluralize', 'sequelize', 'dot-prop'],
   watch: {
