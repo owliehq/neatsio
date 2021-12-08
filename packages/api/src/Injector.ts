1beaa1e000667a21347ed98ba3139902986b55df
Flo <florian@owlie.xyz>
2021-12-08 17:09:28 +0100

    feat(Injector): added error when target class is not yet defined

--- packages/api/src/Injector.ts
+++ packages/api/src/Injector.ts
@@ -5,12 +5,15 @@ class Container {
   private providers: Map<string, any> = new Map()
 
   resolve<T>(target: string | Class<T>): T {
-    const name = typeof target === 'string' ? target : target.name
-
+    let name = ''
+    if(typeof target === 'string'){
+      name = target
+     } else {
+       name = target?.name
+       if(!name) throw new Error('Unable to retrieve name of target class')
+     }
     const resolved = this.providers.get(name)
-
     if (!resolved) throw new Error(`No provider found for ${target}!`)
-
     return resolved
   }
 
