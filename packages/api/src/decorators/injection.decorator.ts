4695af16dab868e10d659aac5b45a47f72ad6125
Flo <florian@owlie.xyz>
2021-12-08 18:51:57 +0100

    fix(Injector): fixed metadata test in injector

--- packages/api/src/decorators/injection.decorator.ts
+++ packages/api/src/decorators/injection.decorator.ts
@@ -2,7 +2,7 @@ import { providerContainer } from '../Injector'
 
 export function Inject(target: any, propertyName: string): any {
   const meta = Reflect.getMetadata('design:type', target, propertyName)
-  if(meta) throw new Error(`metadata for ${propertyName} in ${target.constructor.name} is not available`)
+  if(!meta) throw new Error(`metadata for ${propertyName} in ${target.constructor.name} is not available`)
   Object.defineProperty(target, propertyName, {
     get: () => providerContainer.resolve(meta.name),
     enumerable: true,
