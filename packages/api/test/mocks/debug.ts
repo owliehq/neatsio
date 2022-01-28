62c918ec0bbfbf327baba27bfe4e267856a36ada
Barthélémy Lorette <barthelemy@owlie.xyz>
2022-01-28 16:33:48 +0100

    fix(api): fix debug

--- packages/api/test/mocks/debug.ts
+++ packages/api/test/mocks/debug.ts
@@ -4,10 +4,8 @@ console.log('==================')
 console.log('Start debugging...')
 console.log('==================')
 
-startServer().then(server => {
-  server.listen(3000, () => {
-    console.log('==================')
-    console.log('Server started on port 3000')
-    console.log('==================')
-  })
+startServer(3000).then(server => {
+  console.log('==================')
+  console.log('Server started on port 3000')
+  console.log('==================')
 })
